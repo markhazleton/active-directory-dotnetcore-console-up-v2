fa2d5a61d0d7a796e39c63a8ab19536fc5f89cd1
Mark Hazleton <mark.hazleton@controlorigins.com>
2021-03-09 14:35:58 -0600

    Add Call to Get Groups Update to .Net 5.0

--- up-console/MyInformation.cs
+++ up-console/MyInformation.cs
@@ -10,15 +10,15 @@ using System.Security;
 using System.Threading.Tasks;
 
 namespace up_console
-{
-    public class MyInformation
     {
-        public MyInformation(IPublicClientApplication app, HttpClient client, string microsoftGraphBaseEndpoint)
+    public class MyInformation
         {
+        public MyInformation(IPublicClientApplication app, HttpClient client, string microsoftGraphBaseEndpoint)
+            {
             tokenAcquisitionHelper = new PublicAppUsingUsernamePassword(app);
             protectedApiCallHelper = new ProtectedApiCallHelper(client);
             MicrosoftGraphBaseEndpoint = microsoftGraphBaseEndpoint;
-        }
+            }
 
         protected PublicAppUsingUsernamePassword tokenAcquisitionHelper;
 
@@ -27,7 +27,7 @@ namespace up_console
         /// <summary>
         /// Scopes to request access to the protected Web API (here Microsoft Graph)
         /// </summary>
-        private static string[] Scopes { get; set; } = new string[] { "User.Read", "User.ReadBasic.All" };
+        private static string[] Scopes { get; set; } = new string[] { "User.Read", "User.Read.All", "User.ReadBasic.All" };
 
         /// <summary>
         /// Base endpoint for Microsoft Graph
@@ -39,6 +39,7 @@ namespace up_console
         /// </summary>
         private string WebApiUrlMe { get { return $"{MicrosoftGraphBaseEndpoint}/v1.0/me"; } }
         private string WebApiUrlMyManager { get { return $"{MicrosoftGraphBaseEndpoint}/v1.0/me/manager"; } }
+        private string WebApiUrlMyGroups { get { return $"{MicrosoftGraphBaseEndpoint}/v1.0/me/transitiveMemberOf/microsoft.graph.group"; } }
 
 
         /// <summary>
@@ -46,102 +47,111 @@ namespace up_console
         /// </summary>
         /// <returns></returns>
         public async Task DisplayMeAndMyManagerRetryingWhenWrongCredentialsAsync()
-        {
-            bool again = true;
-            while(again)
             {
+            bool again = true;
+            while (again)
+                {
                 again = false;
                 try
-                {
+                    {
                     await DisplayMeAndMyManagerAsync();
-                }
+                    }
                 catch (ArgumentException ex) when (ex.Message.StartsWith("U/P"))
-                {
+                    {
                     // Wrong user or password
                     WriteTryAgainMessage();
                     again = true;
+                    }
                 }
             }
-        }
 
         private async Task DisplayMeAndMyManagerAsync()
-        {
+            {
             string username = ReadUsername();
             SecureString password = ReadPassword();
 
             AuthenticationResult authenticationResult = await tokenAcquisitionHelper.AcquireATokenFromCacheOrUsernamePasswordAsync(Scopes, username, password);
             if (authenticationResult != null)
-            {
+                {
                 DisplaySignedInAccount(authenticationResult.Account);
 
                 string accessToken = authenticationResult.AccessToken;
                 await CallWebApiAndDisplayResultAsync(WebApiUrlMe, accessToken, "Me");
                 await CallWebApiAndDisplayResultAsync(WebApiUrlMyManager, accessToken, "My manager");
+                await CallWebApiAndDisplayResultAsync(WebApiUrlMyGroups, accessToken, "My Groups");
+                }
             }
-        }
 
         private static void WriteTryAgainMessage()
-        {
+            {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Wrong user or password. Try again!");
             Console.ResetColor();
-        }
+            }
 
         private static SecureString ReadPassword()
-        {
+            {
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Enter your password (no backspace possible)");
             Console.ResetColor();
             SecureString password = new SecureString();
             while (true)
-            {
+                {
                 ConsoleKeyInfo c = Console.ReadKey(true);
                 if (c.Key == ConsoleKey.Enter)
-                {
+                    {
                     break;
-                }
+                    }
                 password.AppendChar(c.KeyChar);
                 Console.Write("*");
-            }
+                }
             Console.WriteLine();
             return password;
-        }
+            }
 
         private static string ReadUsername()
-        {
+            {
             string username;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Enter your username");
             Console.ResetColor();
             username = Console.ReadLine();
             return username;
-        }
+            }
 
         private static void DisplaySignedInAccount(IAccount account)
-        {
+            {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{account.Username} successfully signed-in");
-        }
+            }
 
         private async Task CallWebApiAndDisplayResultAsync(string url, string accessToken, string title)
-        {
+            {
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(title);
             Console.ResetColor();
             await protectedApiCallHelper.CallWebApiAndProcessResultAsync(url, accessToken, Display);
             Console.WriteLine();
-        }
+            }
 
         /// <summary>
         /// Display the result of the Web API call
         /// </summary>
         /// <param name="result">Object to display</param>
         private static void Display(JObject result)
-        {
-            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
             {
-                Console.WriteLine($"{child.Name} = {child.Value}");
+
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+                {
+                try
+                    {
+                    Console.WriteLine($"{child.Name} = {child.Value}");
+                    }
+                catch (Exception ex)
+                    {
+                    Console.WriteLine($"{child.Name} = {ex.Message}");
+                    }
+                }
             }
         }
     }
-}
--- up-console/ProtectedApiCallHelper.cs
+++ up-console/ProtectedApiCallHelper.cs
@@ -10,20 +10,20 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace up_console
-{
+    {
     /// <summary>
     /// Helper class to call a protected API and process its result
     /// </summary>
     public class ProtectedApiCallHelper
-    {
+        {
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="httpClient">HttpClient used to call the protected API</param>
         public ProtectedApiCallHelper(HttpClient httpClient)
-        {
+            {
             HttpClient = httpClient;
-        }
+            }
 
         protected HttpClient HttpClient { get; private set; }
 
@@ -35,42 +35,49 @@ namespace up_console
         /// <param name="accessToken">Access token used as a bearer security token to call the web API</param>
         /// <param name="processResult">Callback used to process the result of the call to the web API</param>
         public async Task CallWebApiAndProcessResultAsync(string webApiUrl, string accessToken, Action<JObject> processResult)
-        {
-            if (!string.IsNullOrEmpty(accessToken))
             {
+            if (!string.IsNullOrEmpty(accessToken))
+                {
                 var defaultRequestHeaders = HttpClient.DefaultRequestHeaders;
                 if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
-                {
+                    {
                     HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                }
+                    }
                 defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
 
                 HttpResponseMessage response = await HttpClient.GetAsync(webApiUrl);
                 if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    JObject result = JsonConvert.DeserializeObject(json) as JObject;
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    processResult(result);
-                }
+                    {
+                    try
+                        {
+                        string json = await response.Content.ReadAsStringAsync();
+                        JObject result = JsonConvert.DeserializeObject(json) as JObject;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        processResult(result);
+                        }
+                    catch
+                        {
+                        }
+
+                    }
                 else
-                {
+                    {
                     Console.ForegroundColor = ConsoleColor.Red;
                     string content = await response.Content.ReadAsStringAsync();
 
                     if (!content.Contains("Resource 'manager' does not exist"))
-                    {
+                        {
                         Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
                         Console.WriteLine($"Content: {content}");
-                    }
+                        }
                     else
-                    {
+                        {
                         Console.ForegroundColor = ConsoleColor.Gray;
                         Console.WriteLine("No manager");
+                        }
                     }
-                }
                 Console.ResetColor();
+                }
             }
         }
     }
-}
